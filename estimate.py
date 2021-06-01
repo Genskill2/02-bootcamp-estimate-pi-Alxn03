195d848e551f57a5236e6f9d5d55eaca9faabcfa
Allen Appukuttan <allenappukuttan@gmail.com>
2021-06-01 20:42:06 +0530

    Exercise_02 v1.0 : wallis estimation of pi and monte carlo estimation of pi

--- estimate.py
+++ estimate.py
@@ -1,5 +1,6 @@
 import math
 import unittest
+import random
 
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
@@ -21,12 +22,31 @@ class TestMC(unittest.TestCase):
         self.assertNotEqual(pi0, pi1, "Two different estimates for PI are exactly the same. This is almost impossible.")
 
         self.assertFalse(abs(pi0 - pi1) > 0.05, "Two different estimates of PI are too different. This should not happen")
-
+        
     def test_accuracy(self):
         for i in range(500, 600):
             pi = monte_carlo(i)
             self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
-        
-    
+
+
+def wallis(n):
+    wallisPI = 1
+    for i in range(1,n+1):
+        wallisPI *= (4 * (i**2)) / (4 * (i**2) - 1)
+    return 2*wallisPI
+
+def monte_carlo(n):
+    circleDarts = 0
+    squareDarts = 0        	
+    for i in range(n):
+        x,y = random.uniform(-1,1), random.uniform(-1,1)
+		
+        if x**2 + y**2 <= 1:
+            circleDarts += 1
+	    
+        squareDarts += 1
+    pi = 4 * (circleDarts/squareDarts)		
+    return pi
+ 	  
 if __name__ == "__main__":
     unittest.main()
